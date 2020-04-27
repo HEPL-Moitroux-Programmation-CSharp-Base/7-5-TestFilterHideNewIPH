db925d57de637e4d77c33ea03ddb12bcb32dd26b
Cecile Moitroux <cecile.moitroux@hepl.be>
2020-04-27 16:38:35 +0200

    Add project files.

---
+++ 7-5 TestFilterHideNewIPH/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTestDatagrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Personne> listpersonne = new ObservableCollection<Personne>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = listpersonne;
+            listpersonne.Add(new Personne("Simpson", "Homer", new DateTime(2051, 5, 21),
+                    @"Images\HomerSimpson.png"));
+            listpersonne.Add(new Personne("Simpson", "Marge", new DateTime(2050, 5, 30),
+                @"Images\MargeSimpson.png"));
+            listpersonne.Add(new Personne("Simpson", "Maggie", new DateTime(2018, 5, 15)));
+            listpersonne.Add(new Personne("Simpson", "Bart", new DateTime(2018, 5, 30)));
+        }
+
+        private void Contains(object sender, FilterEventArgs e)
+        {
+            Personne item = e.Item as Personne;
+            if (item != null)
+            {
+                e.Accepted = true;              
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Programme qui utilise la CollectionViewSource pour cacher les " New Item Placeholder " dans ListBox et ComboBox 
+
+Ce programme utilise la notion de filtrage et de CollectionViewSource pour cacher les lignes "New Item PlaceHolder" dans les ListBox, les ComboBox qui sont non éditables.
+
+Ce phénomène apparait lorsque la même collection est liée à ces composants graphiques et à une datagrid éditable. Dès lors, la ligne prête à être encodée dans la DG apparaît également dans la ListBox et la ComboBox.
+
